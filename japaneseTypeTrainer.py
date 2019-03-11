09fae2c17ad2a509622c75ed263d9755816eb725
IceMasterPT <biogeek1337@gmail.com>
2019-03-11 09:11:05 +0000

    FInished the main menu and loop

--- japaneseTypeTrainer.py
+++ japaneseTypeTrainer.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 import jt
 import time
+import os
 
 timeLimit = 10#10 segundos
 
@@ -16,22 +17,45 @@ def tryMode():
     startTime = time.time()
     kstr = raw_input()
     if int(time.time() - startTime) >= timeLimit:
-        print("You took too long! Meh!")
+        print("You took too long: " +  str(time.time()-startTime) + "seconds")
+
     elif kstr == phaseString:
         print("\ncorrect!")
         print("you took: " + str(time.time()-startTime))
     else:
         print("\nWrong!, it was "+str(phaseString))
+    time.sleep(4)
+    mainMenuLoop()
+
+def printMainMenu():
+    os.system("clear")
+    print("---------------")
+    print(":\t start training :press 1\n")
+    print(":\t exit           :press 0\n")
+    optn = input("write option")
+    if optn == 0:
+        exit(0)
+    elif optn == 1:
+        tryMode()
+    else:
+        print("Uknown option...")
+        mainMenuLoop()
+
+    
+    
 
 
 def mainMenuLoop():
     score = 0
     stage = 0 #0 = normal menu 1 = ingame loop
+    if stage == 0:
+        printMainMenu()
+
     
 
 
 
-tryMode()
+mainMenuLoop()
 
             
     
